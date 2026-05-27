c201570e4178491075017dab52c93f05b62b34ef
Alvin <athanasalvin@gmail.com>
2026-05-27 23:19:17 +0300

    domain updated

--- src/main/java/co/tz/sheriaconnectapi/security/Conf/CorsConfig.java
+++ src/main/java/co/tz/sheriaconnectapi/security/Conf/CorsConfig.java
@@ -11,17 +11,16 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://76.13.14.56:8091",
-                        "http://76.13.14.56:4001",
-                        "http://76.13.14.56:6001",
                         "http://localhost:4001",
                         "http://127.0.0.1:4001",
                         "http://localhost:5173",
                         "http://127.0.0.1:5173",
-                        "http://sheria-connect.co.tz",
-                        "http://www.sheria-connect.co.tz",
-                        "https://sheria-connect.co.tz",
-                        "https://www.sheria-connect.co.tz",
+                        "http://sheriaconnect.co.tz",
+                        "http://www.sheriaconnect.co.tz",
+                        "http://api.sheriaconnect.co.tz",
+                        "https://sheriaconnect.co.tz",
+                        "https://www.sheriaconnect.co.tz",
+                        "https://api.sheriaconnect.co.tz",
                         "10.0.2.2"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
